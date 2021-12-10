6a058967b913cbb32c59011957fc17f082ac6350
paolaagarmun <paola.a.garciamunoz@gmail.com>
2021-12-10 11:05:02 -0500

    created category controller

---
+++ controllers/category.js
@@ -0,0 +1,46 @@
+const Category = require('../Schemas/Category')
+
+const getAllCategories = async (req, res) => {
+    const categories = await Category.find();
+    try {
+        return res.status(200).json(categories)
+    } catch (error) {
+        return res.status(500).json({message: "Couldn't get categories"})
+    }
+}
+
+const getOneCategory = async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findById(id);
+    try {
+        return res.status(200).json(category)
+    } catch (error) {
+        return res.status(500).json({message: "Couldn't get category"})
+    }
+}
+
+const createCategory = async (req, res) => {
+    const createdCategory = await Category.create(req.body);
+    try {
+        return res.status(201).json(createdCategory)
+    } catch (error) {
+        return res.status(500).json({message: "Couldn't create category"})
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params;
+    const categoryToDelete = await Category.findByIdAndDelete(id);
+    try {
+        return res.status(203).json({message: "Deleted category successfully"})
+    } catch (error) {
+        return res.status(500).json({message: "Couldn't delete category"})
+    }
+}
+
+module.exports = {
+    getAllCategories,
+    getOneCategory,
+    createCategory,
+    deleteCategory
+}
